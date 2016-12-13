0e0b55ffc400090f239fad3eaea5caa74df7edeb
igorstetsuk <igorstetsuk25@gmail.com>
2016-12-13 14:59:28 +0200

    workspace

---
+++ HomeWork/src/task/one/Main.java
@@ -0,0 +1,11 @@
+package task.one;
+
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		FlowerBed flower = new FlowerBed();
+	}
+
+}
